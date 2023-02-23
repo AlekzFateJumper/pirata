769f3114ad80cdb88fcdec1227a0e42f0d5642d2
Alexandre <ale.116@gmail.com>
2023-02-23 17:20:15 -0300

    girando, falta ir prá frente

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    private int Health;
-    public float Rotation;
-
     public List<Sprite> sprites;
     public GameObject ship;
-
     public Transform lifeMask;
+    public GameObject explosion;
+    public float speed = 50f;
+
+    private Rigidbody2D shipBody;
+    private int Health;
+    private float Andar;
+    private float Girar;
 
     // Start is called before the first frame update
     void Start()
     {
         Health = 3;
-        Rotation = 0;
+        shipBody = ship.GetComponent<Rigidbody2D>();
     }
 
+    public void Mover(object args)
+    {
+        object[] a = (object[]) args;
+        Andar = (float) a[0];
+        Girar = (float) a[1];
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Multi: " + (Vector3.up * Time.deltaTime * Girar));
+        Debug.Log("Multi: " + (Time.deltaTime * Girar));
+
+        shipBody.transform.Rotate(Vector3.back * Time.deltaTime * Girar * speed);
+
+        if(Andar >= 0){
+            Debug.Log("Andar: " + Andar);
+            Debug.Log("Direção: " + shipBody.transform.up);
+            GetComponent<Rigidbody2D>().AddForce(shipBody.transform.up * Andar * speed);
+        }
     }
 
     void Hit(){
-        ship.GetComponent<SpriteRenderer>.sprite = sprites[--Health];
+        ship.GetComponent<SpriteRenderer>().sprite = sprites[--Health];
         updateLifeBar();
         if(Health <= 0){
             Explode();
@@ -34,7 +52,8 @@ public class ShipController : MonoBehaviour
     }
 
     void Explode(){
-        
+        //var exploding = Instantiate(explosion, transform.position, transform.rotation);
+        //exploding.Sink();
     }
 
     void Exploded(){
@@ -58,5 +77,17 @@ public class ShipController : MonoBehaviour
             break;
         }
         lifeMask.position = pos;
+    }
+
+    void Collide (Collision2D collision) {
+        if (collision.gameObject.CompareTag("CannonBall")) {
+            //var exploding = Instantiate(explosion, transform.position, transform.rotation);
+            //exploding.Weak();
+            Hit();
+        }else if((CompareTag("Chaser") || CompareTag("Player")) && (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Chaser"))){
+            Health = 0;
+            updateLifeBar();
+            Explode();
+        }
     }
 }
