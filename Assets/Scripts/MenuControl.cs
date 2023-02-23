c8366d6f08f209d36be43b7760cf94e39a9523df
Alexandre <ale.116@gmail.com>
2023-02-23 02:04:14 -0300

    menu ready

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -1,31 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuControl : MonoBehaviour
 {
     public GameObject mainMenuObj;
     public GameObject cfgMenuObj;
 
+    public TMP_Text roundTime;
+    public TMP_Text spawnTime;
+
+    private float rTime;
+    private float sTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    goMenu(string menu){
-        if(menu == "cfg"){
-            mainMenuObj.setActive(false);
-            cfgMenuObj.setActive(true);
-        }else{
-            cfgMenuObj.setActive(false);
-            mainMenuObj.setActive(true);
-        }
+        rTime = 60;
+        sTime = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Config(){
+        mainMenuObj.SetActive(false);
+        cfgMenuObj.SetActive(true);
+    }
+    
+    public void Top(){
+        cfgMenuObj.SetActive(false);
+        mainMenuObj.SetActive(true);
+    }
+
+    public void ChangeRoundTime(float time){
+        rTime = time;
+        roundTime.text = TimeSpan.FromSeconds(rTime).ToString(@"m\:ss");
+    }
+
+    public void ChangeSpawnTime(float time){
+        sTime = time;
+        spawnTime.text = TimeSpan.FromSeconds(sTime).ToString(@"m\:ss");
     }
 }
