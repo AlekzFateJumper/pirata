fa073f1482c495ac9cc144333e85151404a1bff8
Alexandre <ale.116@gmail.com>
2023-02-23 07:49:24 -0300

    inicio dos players

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
+    public TMP_Text roundTime;
+
+    private float timeLeft;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = PlayerPrefs.GetInt("rTime");
+        UpdateTime();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        UpdateTime();
+    }
+
+    void UpdateTime(){
+        roundTime.text = TimeSpan.FromSeconds(timeLeft).ToString(@"m\:ss");
     }
 }
