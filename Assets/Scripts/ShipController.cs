5b94357110fbc3cf3fda71e8700ed5e095548fce
Alexandre <ale.116@gmail.com>
2023-02-23 11:52:30 -0300

    ship functions

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    public int Health;
-
+    private int Health;
     private float Rotation;
 
     public List<Sprite> sprites;
-    public 
+    public SpriteRenderer ship;
+
+    public Transform lifeMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,32 @@ public class ShipController : MonoBehaviour
         
     }
 
-    int Hit(float x, float y){
-        return --Health;
+    void Hit(){
+        ship.sprite = sprites[--Health];
+        updateLifeBar();
+        if(Health <= 0){
+            Explode();
+        }
+    }
+
+    void Explode(){
+        
+    }
+
+    void updateLifeBar(){
+        Vector3 pos = new Vector3(0,0,0);
+        switch (Health)
+        {
+            case 2:
+                pos = new Vector3(-.4f,0,0);
+            break;
+            case 1:
+                pos = new Vector3(-.6f,0,0);
+            break;
+            case 0:
+                pos = new Vector3(-1f,0,0);
+            break;
+        }
+        lifeMask.position = pos;
     }
 }
