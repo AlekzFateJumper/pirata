003b7a22cd1dc6ce712443a9f18ed331f2144641
Alexandre <ale.116@gmail.com>
2023-02-23 09:09:34 -0300

    score, game over

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,19 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameController : MonoBehaviour
 {
     public TMP_Text roundTime;
+    public TMP_Text scoreTxt;
 
     private float timeLeft;
+    private int score;
 
     // Start is called before the first frame update
     void Start()
     {
         timeLeft = PlayerPrefs.GetInt("rTime");
+        score = 0;
+
         UpdateTime();
+        UpdateScore();
     }
 
     // Update is called once per frame
@@ -22,9 +28,22 @@ public class GameController : MonoBehaviour
     {
         timeLeft -= Time.deltaTime;
         UpdateTime();
+        UpdateScore();
+        if(timeLeft <= 0){
+            GameOver();
+        }
     }
 
     void UpdateTime(){
         roundTime.text = TimeSpan.FromSeconds(timeLeft).ToString(@"m\:ss");
+    }
+
+    void UpdateScore(){
+        scoreTxt.text = "Pontos: " + score.ToString();
+    }
+
+    void GameOver(){
+        PlayerPrefs.SetInt("score", score);
+        SceneManager.LoadScene("Game");
     }
 }
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -13,7 +13,7 @@ public class ShipController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Health = 4;
+        Health = 3;
         Rotation = 0;
     }
 
