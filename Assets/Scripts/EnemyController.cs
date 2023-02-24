a9ce8f0f6ea96eb7f00282c3a83c0bbbd976165a
Alexandre <ale.116@gmail.com>
2023-02-24 18:35:44 -0300

    .

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,7 +6,7 @@ public class EnemyController : MonoBehaviour
 {
     public List<Sprite> sprites;
 
-    void Init()
+    public void Init()
     {
         ShipController shipCtrl = GetComponent<ShipController>();
         shipCtrl.sprites = sprites;
