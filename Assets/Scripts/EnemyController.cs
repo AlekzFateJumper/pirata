d09be603f2e26b558a0b9254af16ae1995d2479c
Alexandre <ale.116@gmail.com>
2023-02-26 00:53:08 -0300

    codigo muito grande e pesado, vou reescrever

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,6 +14,7 @@ public class EnemyController : MonoBehaviour
     private float giro;
     private float veloc;
     private float deltaToPlayer;
+    private float anguloDesvio;
     private List<Collider2D> nearObjs;
 
     public void Init(List<Sprite> sprites)
@@ -24,13 +25,15 @@ public class EnemyController : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         shipColliderId = shipCtrl.ship.GetComponent<Collider2D>().GetInstanceID();
         desviar = false;
+        anguloDesvio = 0f;
         deltaToPlayer = 0f;
+        nearObjs = new List<Collider2D>();
         giro = 0f;
         veloc = 0f;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         playerPos = player.transform.position;
 
@@ -43,7 +46,7 @@ public class EnemyController : MonoBehaviour
         args[0] = veloc;
         args[1] = giro;
         shipCtrl.Mover(args);
-        // Debug.Log("Desviar: " + desviar + "\r\nMover: " + veloc + " / " + giro);
+        Debug.Log("Desviar: " + desviar + "\r\nMover: " + veloc + " / " + giro + "\r\nObjs: " + nearObjs.Count);
     }
 
     void MoveControl(){
@@ -65,7 +68,7 @@ public class EnemyController : MonoBehaviour
                     giro = sideDelta / 100;
                     if(shipCtrl.cannonWait[0] <= 0f) shipCtrl.TiroLateral(true);
                 }else{
-                    giro = Mathf.Sign(sideDelta);
+                    giro = sideDelta == 0f ? 0 : Mathf.Sign(sideDelta);
                 }
             } else
             if ( InRange( deltaToPlayer, 45, 180, false ) ) {
@@ -74,7 +77,7 @@ public class EnemyController : MonoBehaviour
                     giro = sideDelta / 100;
                     if(shipCtrl.cannonWait[0] <= 0f) shipCtrl.TiroLateral(false);
                 }else{
-                    giro = Mathf.Sign(sideDelta);
+                    giro = sideDelta == 0f ? 0 : Mathf.Sign(sideDelta);
                 }
             }
         } else {
@@ -85,21 +88,24 @@ public class EnemyController : MonoBehaviour
 
     void CheckForWalls(){
         Collider2D collider = getNearestObj();
+        Debug.Log("Mais perto: " + collider.name);
 
-        var angle = deltaAngle(collider.transform.position);
         var dist = collider.Distance(shipCtrl.ship.GetComponent<Collider2D>());
+        var angle = deltaAngle(collider.transform.position);
         var abs = Mathf.Abs(angle);
 
-        if ( abs <= 75 ){
+        if ( abs <= 85 ){
             desviar = true;
-            if(abs > 40){
-                giro = - Mathf.Sign(angle);
-                veloc = dist.distance > 1.2f ? 1f : 0f;
-            }else if(abs > 20){
-                giro = - Mathf.Sign(angle);
-                veloc = dist.distance > 1.2f ? 0.5f : 0f;
+            giro = (abs > 1f ? Mathf.Sign(angle) : angle);
+            if(abs < 20){
+                veloc = dist.distance > 1.5f ? .2f : 0f;
+            } else if(abs < 45){
+                veloc = dist.distance > 1.5f ? .5f : 0f;
+            } else {
+                veloc = dist.distance > 1.5f ? 1f : 0f;
             }
         }else{
+            Debug.Log("Mais que 85.");
             desviar = false;
         }
     }
@@ -114,7 +120,44 @@ public class EnemyController : MonoBehaviour
                 collider = obj;
             }
         }
+        Debug.Log(collider == null ? "Sem parede" : "Com parede");
         return collider;
+    }
+
+    float freeAngle(bool nearPlayer = false){
+        float way = shipCtrl.ship.transform.rotation.z;
+        float playerAngle = getAngle(playerPos);
+        float maxRot = Mathf.Infinity;
+        float nearAngle = way;
+        List<float> angles = new List<float>();
+        foreach(var obj in nearObjs) {
+            float angle = getAngle(Vector3 obj.transform.position);
+            float inv = invertAngle(angle);
+            float near = Mathf.DeltaAngle(inv, playerAngle);
+            if( near < maxRot ){
+                maxRot = near;
+                nearAngle = inv;
+            }
+            angles.Add(angle);
+        }
+        angles = angles.OrderBy(item => item.Value).ToList();
+        float bigGapSize = Mathf.Infinity;
+        float[] bigGap = {0f, 0f};
+        float[] nearGap = {0f, 0f};
+        float lastAngle = angles[angles.Count - 1];
+        foreach(float angle in angles){
+            float[] gap = {lastAngle, angle};
+            float size = Mathf.DeltaAngle(angle, lastAngle);
+            if (size < bigGap){
+                bigGap = gap;
+                bigGapSize = size
+            }
+            if(nearPlayer && InRange(player, gap[0], gap[1], true)){
+                nearGap = gap;
+            }
+            
+        }
+        return way;
     }
 
     float getAngle(Vector3 target){
@@ -129,6 +172,10 @@ public class EnemyController : MonoBehaviour
         return Mathf.DeltaAngle(getAngle(target), shipCtrl.ship.transform.eulerAngles.z);
     }
 
+    float invertAngle(float a){
+        return a > 0 ? a - 180 : a + 180;
+    }
+
     float getGiroToPlayer(){
         // Calcula giro para olhar player.
         deltaToPlayer = deltaAngle(playerPos);
@@ -138,9 +185,17 @@ public class EnemyController : MonoBehaviour
         return g;
     }
 
+    float getGiroToAngle(float angle){
+        // Calcula giro para olhar para o angulo.
+        deltaToAngle = deltaAngle(angle);
+        float abs = Mathf.Abs(deltaToAngle);
+        float g = Mathf.Sign(deltaToAngle);
+        if(abs < 25f) g = g / ((25f - abs)/10f + 1f);
+        return g;
+    }
+
     void OnTriggerEnter2D(Collider2D collider){
         if( collider.GetInstanceID() == shipColliderId ||
-            collider.name == "Main Camera" ||
             collider.tag == "Respawn" ||
             collider.tag == "Player"
         ) return;
@@ -152,7 +207,6 @@ public class EnemyController : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D collider){
         if( collider.GetInstanceID() == shipColliderId ||
-            collider.name == "Main Camera" ||
             collider.tag == "Respawn" ||
             collider.tag == "Player"
         ) return;
@@ -163,6 +217,10 @@ public class EnemyController : MonoBehaviour
     }
 
     bool InRange(float n, float min, float max, bool eq = false){
+        if(min > max && min >= 0 && max <= 0){
+            if(eq) return n >= min || n <= max;
+            return n > min || n < max;
+        }
         if(eq) return n >= min && n <= max;
         return n > min && n < max;
     }
